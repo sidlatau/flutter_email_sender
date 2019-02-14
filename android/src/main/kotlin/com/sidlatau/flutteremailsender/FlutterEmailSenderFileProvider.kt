8d3a85eb9576c4da531ca90009958638e17bb779
Muchamad Arifin Dwi P <apin.klas@gmail.com>
2019-02-14 19:06:22 +0700

    upgrade to androidx (#12)
    * upgrade to androidx

--- android/src/main/kotlin/com/sidlatau/flutteremailsender/FlutterEmailSenderFileProvider.kt
+++ android/src/main/kotlin/com/sidlatau/flutteremailsender/FlutterEmailSenderFileProvider.kt
@@ -1,5 +1,5 @@
 package com.sidlatau.flutteremailsender
 
-import android.support.v4.content.FileProvider
+import androidx.core.content.FileProvider
 
 class FlutterEmailSenderFileProvider : FileProvider()
