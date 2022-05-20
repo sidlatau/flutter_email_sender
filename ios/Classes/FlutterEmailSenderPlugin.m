c4cbcea130096ae083c828481c813be7904100d7
fachrifaul <fachri.febrian@bukalapak.com>
2022-05-20 13:24:44 +0700

    support Objective-C project with Podfile no use_frameworks

--- ios/Classes/FlutterEmailSenderPlugin.m
+++ ios/Classes/FlutterEmailSenderPlugin.m
@@ -1,5 +1,12 @@
 #import "FlutterEmailSenderPlugin.h"
+#if __has_include(<flutter_email_sender/flutter_email_sender-Swift.h>)
 #import <flutter_email_sender/flutter_email_sender-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_email_sender-Swift.h"
+#endif
 
 @implementation FlutterEmailSenderPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
